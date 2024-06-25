559f088e0226840a1190c169bd0b09a0480bf01e
Pablo Marotta <pablomarotta.pm@gmail.com>
2024-06-25 11:08:09 -0300

    creando el authRedirect y limpiando el main

---
+++ lib/screens/authRedirect.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:smartup_challenge/screens/home/home.dart';
+
+class AuthRedirect extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
